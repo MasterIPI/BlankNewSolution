43c8efcbb28cffcc4684735343ad65e6a113288d
Vlad Ivanov <iwinner521@gmail.com>
2018-02-26 23:01:16 +0200

    sync

--- BlankNewSolution.Core/ViewModels/MainViewModel.cs
+++ BlankNewSolution.Core/ViewModels/MainViewModel.cs
@@ -12,7 +12,6 @@ namespace BlankNewSolution.Core.ViewModels
         public override Task Initialize()
         {
             //TODO: Add starting logic here
-		    
             return base.Initialize();
         }
         
